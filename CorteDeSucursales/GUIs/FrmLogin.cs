5eeb89477ca15ff00abd551c396a6d97ef70e68a
desarrollocorrales <desarrollo@loscorrales.com.mx>
2015-11-13 08:51:21 -0600

    Creación del Proyecto
    Se maqueta el proyecto con las carpetas siguientes:
    - GUI. Para los formularios.
    - Modelos. Para las clases que se necesiten.
    - Entity. Para la capa de base de datos.
    - Recusros. Para los recursos multimedia.

---
+++ CorteDeSucursales/GUIs/FrmLogin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace CorteDeSucursales.GUIs
+{
+    public partial class FrmLogin : DevExpress.XtraEditors.XtraForm
+    {
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CorteDeSucursales/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos atributos para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("CorteDeSucursales")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("CorteDeSucursales")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible como false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM. Si necesita obtener acceso a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible como true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+[assembly: Guid("34e586b8-9f14-4e6e-8295-516af88fadff")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o establecer como predeterminados los números de versión de compilación y de revisión 
+// mediante el asterisco ('*'), como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
